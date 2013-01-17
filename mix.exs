87b36283a655c3e7b9c832792e33b383977312ca
meh <meh@schizofreni.co>
2013-01-18 00:36:45 +0100

    project: improve converting tasks

--- mix.exs
+++ mix.exs
@@ -55,93 +55,188 @@ defmodule Mix.Tasks.Athena do
   end
 end
 
-defmodule Mix.Tasks.Athena do
-  defmodule Skills do
-    use    Mix.Task
-    import Fjalar.Mixfile.Helpers
+defmodule Mix.Tasks.Athena.Skill do
+  use    Mix.Task
+  import Mix.Tasks.Athena
 
-    defp hit(value) do
-      if value == 6 do
-        :single
-      else
-        :repeated
+  defp hit(value) do
+    if value == 6 do
+      :single
+    else
+      :repeated
+    end
+  end
+
+  defp inf(value) do
+    [{0, :passive}, {1, :enemy}, {2, :place}, {4, :self}, {16, :friend}, {32, :trap}][value]
+  end
+
+  defp element(value) do
+    elements = [{0, :neutral}, {1, :water}, {2, :earth}, {3, :fire}, {4, :wind}, {5, :poison},
+                {6, :holy}, {7, :dark}, {8, :ghost}, {9, :undead}, {-1, :weapon}, {-2, :endowed},
+                {-3, :random}]
+
+    if List.member?(String.graphemes(value), ":") do
+      Enum.reduce String.split(value, ":"), [], fn(num, acc) ->
+        [elements[binary_to_integer(num)] | acc]
       end
+    else
+      elements[binary_to_integer(value)]
     end
+  end
 
-    def inf(value) do
-      [{0, :passive}, {1, :enemy}, {2, :place}, {4, :self}, {16, :friend}, {32, :trap}][value]
-    end
+  defp nk(value) do
+    use Bitwise
 
-    def element(value) do
-      elements = [{0, :neutral}, {1, :water}, {2, :earth}, {3, :fire}, {4, :wind}, {5, :poison},
-                  {6, :holy}, {7, :dark}, {8, :ghost}, {9, :undead}, {-1, :weapon}, {-2, :endowed},
-                  {-3, :random}]
-
-      if List.member?(String.codepoints(value), ":") do
-        Enum.reduce String.split(value, ":"), [], fn(num, acc) ->
-          [elements[binary_to_integer(num)] | acc]
+    Enum.reduce [{0x01, :none}, {0x02, :splash}, {0x04, :split}, {0x08, :ignore_damage_cards},
+                 {0x10, :ignore_elements}, {0x20, :ignore_defense}, {0x40, :ignore_flee},
+                 {0x80, :ignore_defense_cards}], [],
+      fn({bit, name}, acc) ->
+        if value &&& bit != 0 do
+          [name | acc]
+        else
+          acc
         end
-      else
-        elements[binary_to_integer(value)]
       end
+  end
+
+  defp inf2(value) do
+    use Bitwise
+
+    Enum.reduce [{0x0001, :quest}, {0x0002, :npc}, {0x0003, :wedding}, {0x0008, :spirit},
+                 {0x0010, :guild}, {0x0020, :song}, {0x0040, :ensemble}, {0x0080, :trap},
+                 {0x0100, :self}, {0x0200, :not_self}, {0x0400, :party}, {0x0800, :guild},
+                 {0x1000, :no_enemies}, {0x2000, :ignore_land_protecto}, {0x4000, :chorus}], [],
+      fn({bit, name}, acc) ->
+        if value &&& bit != 0 do
+          [name | acc]
+        else
+          acc
+        end
+      end
+  end
+
+  def run([path]) do
+    csv path, fn(data) ->
+      IO.puts "#{Enum.at! data, 16} > #{String.strip(Enum.at!(data, 15))}"
+      IO.puts "01| ID: #{Enum.at! data, 0}"
+      IO.puts "02| Range: #{Enum.at! data, 1}"
+      IO.puts "03| Hit: #{hit(binary_to_integer(Enum.at!(data, 2)))}"
+      IO.puts "04| Inf: #{inf(binary_to_integer(Enum.at!(data, 3)))}"
+
+      case element(Enum.at!(data, 4)) do
+        list = [_|_] ->
+          IO.puts "05| Elements: #{Enum.join Enum.map(by_level(list), fn({level, element}) ->
+              "#{level}[#{element}]"
+            end), " "}"
+
+        element -> IO.puts "05| Element: #{element}"
+      end
+
+      IO.puts "06| Skill Damage: #{Enum.join nk(binary_to_integer(String.replace(Enum.at!(data, 5), "0x", ""), 16)), " "}"
+      IO.puts "07| Effect Range: #{Enum.at!(data, 6)}"
+      IO.puts "08| Max Level: #{Enum.at!(data, 7)}"
+      IO.puts "09| Hits: #{Enum.at!(data, 8)}"
+      IO.puts "10| Cast Interruptible: #{Enum.at!(data, 9)}"
+      IO.puts "11| Defense Reduction: #{Enum.at!(data, 10)}"
+      IO.puts "12| Inf2: #{Enum.join inf2(binary_to_integer(String.replace(Enum.at!(data, 11), "0x", ""), 16)), " "}"
+      IO.puts "13| Max Count: #{Enum.at!(data, 12)}"
+      IO.puts "14| Type: #{Enum.at!(data, 13)}"
+      IO.puts "15| Knockback: #{Enum.at!(data, 14)}"
+
+      IO.puts ""
+    end
+  end
+end
+
+defmodule Mix.Tasks.Athena.Skill.Cast do
+  use    Mix.Task
+  import Mix.Tasks.Athena
+
+  defp prepare(value) do
+    if List.member?(String.graphemes(value), ":") do
+      Enum.reduce String.split(value, ":"), [], fn(num, acc) ->
+        [binary_to_integer(num) | acc]
+      end
+    else
+      binary_to_integer(value)
+    end
+  end
+
+  def run([path, db]) do
+    {:ok, names} = csv_with db, [], fn(data, acc) ->
+      [{Enum.at!(data, 0), {Enum.at!(data, 16), String.strip(Enum.at!(data, 15))}} | acc]
     end
 
-    def nk(value) do
-      use Bitwise
+    csv path, fn(data) ->
+      if names[Enum.at!(data, 0)] do
+        IO.puts "#{elem names[Enum.at!(data, 0)], 0} > #{elem names[Enum.at!(data, 0)], 1}"
+        IO.puts "01| ID: #{Enum.at!(data, 0)}"
 
-      Enum.reduce [{0x01, :none}, {0x02, :splash}, {0x04, :split}, {0x08, :ignore_damage_cards},
-                   {0x10, :ignore_elements}, {0x20, :ignore_defense}, {0x40, :ignore_flee},
-                   {0x80, :ignore_defense_cards}], [],
-        fn({bit, name}, acc) ->
-          if value &&& bit != 0 do
-            [name | acc]
-          else
-            acc
-          end
-        end
-    end
+        case prepare(Enum.at!(data, 1)) do
+          list = [_|_] ->
+            IO.puts "02| Casting Time: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
 
-    def inf2(value) do
-      use Bitwise
-
-      Enum.reduce [{0x0001, :quest}, {0x0002, :npc}, {0x0003, :wedding}, {0x0008, :spirit},
-                   {0x0010, :guild}, {0x0020, :song}, {0x0040, :ensemble}, {0x0080, :trap},
-                   {0x0100, :self}, {0x0200, :not_self}, {0x0400, :party}, {0x0800, :guild},
-                   {0x1000, :no_enemies}, {0x2000, :ignore_land_protecto}, {0x4000, :chorus}], [],
-        fn({bit, name}, acc) ->
-          if value &&& bit != 0 do
-            [name | acc]
-          else
-            acc
-          end
-        end
-    end
-
-    def run([path]) do
-      csv path, fn(data) ->
-        IO.puts "#{Enum.at! data, 16} > #{String.strip(Enum.at!(data, 15))}"
-        IO.puts "01| ID: #{Enum.at! data, 0}"
-        IO.puts "02| Range: #{Enum.at! data, 1}"
-        IO.puts "03| Hit: #{hit(binary_to_integer(Enum.at!(data, 2)))}"
-        IO.puts "04| Inf: #{inf(binary_to_integer(Enum.at!(data, 3)))}"
-
-        case element(Enum.at!(data, 4)) do
-          list = [_|_] ->  IO.puts "05| Elements: #{Enum.join(list, " ")}"
-          element      ->  IO.puts "05| Element: #{element}"
+          time -> IO.puts "02| Casting Time: #{time}"
         end
 
-        IO.puts "06| Skill Damage: #{Enum.join nk(binary_to_integer(String.replace(Enum.at!(data, 5), "0x", ""), 16)), " "}"
-        IO.puts "07| Effect Range: #{Enum.at!(data, 6)}"
-        IO.puts "08| Max Level: #{Enum.at!(data, 7)}"
-        IO.puts "09| Hits: #{Enum.at!(data, 8)}"
-        IO.puts "10| Cast Interruptible: #{Enum.at!(data, 9)}"
-        IO.puts "11| Defense Reduction: #{Enum.at!(data, 10)}"
-        IO.puts "12| Inf2: #{Enum.join inf2(binary_to_integer(String.replace(Enum.at!(data, 11), "0x", ""), 16)), " "}"
-        IO.puts "13| Max Count: #{Enum.at!(data, 12)}"
-        IO.puts "14| Type: #{Enum.at!(data, 13)}"
-        IO.puts "15| Knockback: #{Enum.at!(data, 14)}"
+        case prepare(Enum.at!(data, 2)) do
+          list = [_|_] ->
+            IO.puts "03| After Cast Act Delay: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
 
-        IO.puts "---"
+          time -> IO.puts "03| After Cast Act Delay: #{time}"
+        end
+
+        case prepare(Enum.at!(data, 3)) do
+          list = [_|_] ->
+            IO.puts "04| After Cast Walk Delay: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
+
+          time -> IO.puts "04| After Cast Walk Delay: #{time}"
+        end
+
+        case prepare(Enum.at!(data, 4)) do
+          list = [_|_] ->
+            IO.puts "05| Duration1: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
+
+          time -> IO.puts "05| Duration1: #{time}"
+        end
+
+        case prepare(Enum.at!(data, 5)) do
+          list = [_|_] ->
+            IO.puts "06| Duration2: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
+
+          time -> IO.puts "06| Duration2: #{time}"
+        end
+
+        case prepare(Enum.at!(data, 6)) do
+          list = [_|_] ->
+            IO.puts "07| Cooldown: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
+
+          time -> IO.puts "07| Cooldown: #{time}"
+        end
+
+        case prepare(Enum.at!(data, 7)) do
+          list = [_|_] ->
+            IO.puts "08| Fixed Casting Time: #{Enum.join Enum.map(by_level(list), fn({level, time}) ->
+              "#{level}[#{time}]"
+            end), " "}"
+
+          time -> IO.puts "08| Fixed Casting Time: #{time}"
+        end
+
+        IO.puts ""
       end
     end
   end
